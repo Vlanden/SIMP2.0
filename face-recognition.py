b5438b1de6a651502101ea494e8359f63767ec17
Suamo456 <genarocaguamo2000@gmail.com>
2024-04-12 14:32:56 -0600

    Create face-recognition.py

---
+++ face-recognition.py
@@ -0,0 +1,39 @@
+import cv2
+import mediapipe as mp
+import face_recognition as fr
+import numpy
+import os 
+import math
+
+#Paths 
+
+SalidaRostros = "C:/Users/GenaroSigalaAlvarado/Documents/GitHub/SIMP2.0/Rostros"
+ChecadorRostros = "C:/Users/GenaroSigalaAlvarado/Documents/GitHub/SIMP2.0/Rostros/"
+
+#por si se ocupa informacion local
+info = []
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
