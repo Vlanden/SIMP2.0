7fe7b57ca2cb478a79c072e5a1a24c55a85a78ef
EnriqueGR123 <118323832+EnriqueGR123@users.noreply.github.com>
2024-05-05 18:55:09 -0600

    recuperacion
    recuperacion de contraseña

--- PHP/recupera.php
+++ PHP/recupera.php
@@ -1,10 +1,22 @@
 <?php
     require_once("conexionBD.php");
     $name = $_POST['email'];
-    $pass=password_hash($_POST['pass'], PASSWORD_DEFAULT,['cost' => 15]);
-    $sql = "UPDATE simp SET pass='$pass' WHERE correo='$name'";
-    $envio = mysqli_query($conexion, $sql);
-    header('Location:recuperacion.html');
+    $pass = $_POST['pass'];
+    $pass1 = $_POST['pass1'];
+    if(!empty($pass) and !empty($pass1) and !empty($email)  ){
+        echo"Campos vacios";
+    }else{
+        if($pass===$pass1){
+            $pass=password_hash($pass, PASSWORD_DEFAULT,['cost' => 15]);
+            $sql = "UPDATE simp SET pass='$pass' WHERE correo='$name'";
+            $envio = mysqli_query($conexion, $sql);
+            header('Location:recuperacion.html');
+        }else{
+            echo"Contraseña no coincide";
+        }
+    }
+    
+    
    
 
 
