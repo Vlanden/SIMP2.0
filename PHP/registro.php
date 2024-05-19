41f00677505976df86356f99b28f9693da0adab7
Vlanden <99295243+Vlanden@users.noreply.github.com>
2024-05-19 12:35:44 -0600

    Solo faltaria confirmarorden con la bd

--- PHP/registro.php
+++ PHP/registro.php
@@ -1,63 +1,11 @@
 <?php
 session_start();
-include_once "conexionBD.php";
-
+include_once "php/conexionBD.php";
 // Recoger los datos del formulario
-$name=$_POST['name'];
-$email=$_POST["correo"];
+$name = $_POST['n'];
+$email=$_POST["c"];
 $id = isset($_POST['ID']) ? $_POST['ID'] : null;
-$encrypt_pass=$_POST["pass"];
-$confirmar_pass = $_POST['confirmar_pass'];
-
-/*
-// Función para generar un ID único
-function generarIDUnico($conexion) {
-    $id = uniqid('user_');
-    // Aquí podrías agregar lógica adicional para verificar que el ID no se repita
-            // Consulta SQL para seleccionar todos los valores de la tabla "id"
-        $resultado = mysqli_query($conexion,"SELECT * FROM id");
-
-        // Verificar si hay resultados
-        if (mysqli_num_rows($resultado)) {
-            // Recorrer los resultados y mostrarlos
-            while ($fila = $resultado->) {
-                fetch_assoc()
-                echo "ID: " . $fila["id"] . "<br>";
-            }
-        } else {
-            echo "No se encontraron registros en la tabla 'id'.";
-        }
-
-    return $id;
-}
-
-// Verificar si se ha enviado un ID
-$id = isset($_POST['ID']) ? $_POST['ID'] : generarIDUnico($conexion);
-*/
-
-
-
-$email=stripcslashes($email);
-$encrypt_pass=stripcslashes($encrypt_pass);
-$confirmar_pass=stripcslashes($confirmar_pass);
-
-
-// Seguridad anti inyección SQL
-$nombre = mysqli_real_escape_string($conexion, $nombre);
-$correo = mysqli_real_escape_string($conexion, $correo);
-$contrasena = mysqli_real_escape_string($conexion, $contrasena);
-
-// Verificar que las contraseñas coincidan y encriptar la contraseña
-
-if ($encrypt_pass !== $confirmar_pass) {
-    die('Las contraseñas no coinciden.');//die es un exit()Detiene la ejecucion del programa
-    
-} else {
-    $encrypt_pass = password_hash($encrypt_pass, PASSWORD_DEFAULT,['cost' => 15]);
-}
-
-
-if ( !empty($email) ) {
+if ( !empty($fname)  && !empty($email) ) {
     if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
         $sql = mysqli_query($conexion, "SELECT * FROM simp WHERE correo = '{$email}'");
         if (mysqli_num_rows($sql) > 0) {
@@ -77,11 +25,11 @@ if ( !empty($email) ) {
                     
                     $img_name = $name .'.png';
                     //Mover imagen a la carpeta deseada
-                    if (move_uploaded_file($tmp_name, "\SIMP2.0\Rostro" . $img_name)) {
-                        $encrypt_pass = password_hash($_POST['pass'], PASSWORD_DEFAULT,['cost' => 15]);
+                    if (move_uploaded_file($tmp_name, "SIMP2.0/Rostros/" . $img_name)) {
+                        $encrypt_pass = password_hash($_POST['P'], PASSWORD_DEFAULT,['cost' => 15]);
                         
                         $img_name = $name .'.png';
-                        if (move_uploaded_file($tmp_name, "images/" . $img_name)) {
+                        if (move_uploaded_file($tmp_name, "SIMP2.0/Rostros/" . $img_name)) {
                             if ($id !== null) {
                                 // Insertar con ID
                                 $insert_query = mysqli_query($conexion, "INSERT INTO simp(nom, correo, id, pass, img)
@@ -95,7 +43,7 @@ if ( !empty($email) ) {
                                 $select_sql2 = mysqli_query($conexion, "SELECT * FROM simp WHERE correo = '{$email}'");
                                 if (mysqli_num_rows($select_sql2) > 0) {
                                     $result = mysqli_fetch_assoc($select_sql2);
-                                    $_SESSION['unique_id'] = $result['unique_id'];
+                                    $_SESSION['ID'] = $result['ID'];
                                     header("Location:proyecto\usuarioHTML\InicioSesion.html");
                                     echo "Proceso Exitoso";
                                 }{
